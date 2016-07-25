5187ae9918c516fcced3686e56e2b9d0cedd53c4
laojianke <laojianke@gmail.com>
2016-07-25 22:58:13 +0800

    Update rotateLog.pl

--- rotateLog.pl
+++ rotateLog.pl
@@ -20,37 +20,38 @@ else {
 sub rotateLog($) {
     my ($params) = @_;
     my 	$LOGREF = $params->{LOGNAME};
-	my  $MAXLOGSIZE = $params->{MAXLOGSIZE};
-	my  $MAXFILENUM = $params->{MAXFILENUM};
-	my  $SVCCMD = $params->{SVCCMD};
-	foreach my $LOGNAME( @{$LOGREF} ) {
-        my $logSize = ( -f $LOGNAME ) ? (-s $LOGNAME):0;
-        if ( $logSize >= $MAXLOGSIZE ) {
-            my $fileId = 0;
-            my @fileList = glob( $LOGNAME . '*' );
-            while (<@fileList>) {
-                if (/\.(\d+)$/) {
-                    if ( $1 > $fileId ) {
-                        $fileId = $1;
-                    }
+    my  $MAXLOGSIZE = $params->{MAXLOGSIZE};
+    my  $MAXFILENUM = $params->{MAXFILENUM};
+    my  $SVCCMD = $params->{SVCCMD};
+    foreach my $LOGNAME( @{$LOGREF} ) {
+    my $logSize = ( -f $LOGNAME ) ? (-s $LOGNAME):0;
+    if ( $logSize >= $MAXLOGSIZE ) {
+        my $fileId = 0;
+        my @fileList = glob( $LOGNAME . '*' );
+        while (<@fileList>) {
+            if (/\.(\d+)$/) {
+                if ( $1 > $fileId ) {
+                    $fileId = $1;
                 }
             }
-            while ( $fileId >= 0 ) {
-	    		if($fileId >= $MAXFILENUM) {
-	    			unlink $LOGNAME.".".$fileId if( -e $LOGNAME.".".$fileId);	
-	    		}
-	    		else {
-	    			my ($oldFileName, $newFileName);
-	    			$oldFileName = $LOGNAME . "." . $fileId;
-	    			$newFileName = $LOGNAME . "." . ( $fileId + 1 );
-	    			rename($oldFileName,$newFileName) if ( -f $oldFileName );
-	    		}
-	    	$fileId--;	
-            }
-	    rename($LOGNAME,$LOGNAME.".0");
-		system($SVCCMD);
         }
-	}
+        while ( $fileId >= 0 ) {
+   		if($fileId >= $MAXFILENUM) {
+    			unlink $LOGNAME.".".$fileId if( -e $LOGNAME.".".$fileId);	
+    		}
+    		else {
+    			my ($oldFileName, $newFileName);
+	    		$oldFileName = $LOGNAME . "." . $fileId;
+	    		$newFileName = $LOGNAME . "." . ( $fileId + 1 );
+	    		rename($oldFileName,$newFileName) if ( -f $oldFileName );
+	    	}
+	    	$fileId--;	
+        }
+	rename($LOGNAME,$LOGNAME.".0");
+	#Let syslog process refresh file handler. If not syslog, please change $SVCCMD to you command to refresh process.
+	system($SVCCMD);
+        }
+    }
 }
   #main
 rotateLog($params);
